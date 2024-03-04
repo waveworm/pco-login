ec82b6568b6a747550e4ab502b4dc0f450992bc4
waveworm <jim.e.arnold@gmail.com>
2024-03-04 15:00:53 -0500

    Changes to use .env file

--- form_submissions.py
+++ form_submissions.py
@@ -1,18 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 import os
 import csv
 
-""" Your login information below. It's probably best to store this in environment variables."""
-# EMAIL = 'email@example.com'
-# PASSWORD = 'password'
+# Load environment variables from .env file
+load_dotenv()
 
-""" Uncomment below and comment out above if you're using environment variables"""
+# Retrieve email and password from environment variables
 EMAIL = os.environ.get('pcoemail')
 PASSWORD = os.environ.get('pcopass')
 FORM_ID = '9041'  # The ID of the Form
-""" Internal URL """
 
+# URL for downloading the form submissions
 SCRAPEURL = 'https://people.planningcenteronline.com/forms/' + FORM_ID + '/submissions/export.csv'
 
 headers = {
@@ -20,22 +20,30 @@ headers = {
                   'Chrome/67.0.3396.99 Safari/537.36'
 }
 
-login_data = dict(utf8=None,
-                  email=EMAIL,
-                  password=PASSWORD,
-                  authenticity_token=None)
+# Initialize login data with placeholders for authenticity_token and utf8
+login_data = {
+    'utf8': None,
+    'email': EMAIL,
+    'password': PASSWORD,
+    'authenticity_token': None
+}
 
 with requests.Session() as s:
+    # Get the login page to retrieve the authenticity_token and utf8 values
     url = 'https://accounts.planningcenteronline.com/login'
     r = s.get(url, headers=headers)
     soup = BeautifulSoup(r.content, 'html.parser')
     login_data['authenticity_token'] = soup.find('input', attrs={'name': 'authenticity_token'})['value']
     login_data['utf8'] = soup.find('input', attrs={'name': 'utf8'})['value']
+
+    # Log in to the site
     r = s.post(url, data=login_data, headers=headers)
 
+    # Download the CSV file with form submissions
     page = s.get(SCRAPEURL)
     csv_file = page.content.decode('utf-8')
 
+    # Process the CSV file
     csv_reader = csv.reader(csv_file.splitlines(), delimiter=',')
     line_count = 0
     for row in csv_reader:
